80eaffe5d0154f025d84006f00fb97b22d1c3765
nikue <nikueh@gmail.com>
2015-03-09 20:09:38 +0200

    Update schema.rb

--- lib/acts_as_shopping_cart/schema.rb
+++ lib/acts_as_shopping_cart/schema.rb
@@ -10,6 +10,7 @@ module ActsAsShoppingCart
       string  :item_type      # Holds the type of the object, for polymorphism
       integer :price_cents, default: 0, null: false  # Holds the price of the item
       string  :price_currency, default: "USD", null: false # Holds the currency for the price
+      string  :currency, default: "USD", null: false # Holds the currency for the price
     end
   end
 end
